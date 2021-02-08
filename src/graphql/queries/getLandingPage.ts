9a51ffb10e219937555a584d9f8dfbd04f0841f5
André <andrevictor50@gmail.com>
2021-02-08 08:13:46 -0300

    Section dynamic

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -21,10 +21,22 @@ const GET_LANDING_PAGE = /* GraphQL */ `
     }
   }
 
+  fragment section on LandingPage {
+    section {
+      title
+      description
+      image {
+        url
+        alternativeText
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
       ...logo
       ...header
+      ...section
     }
   }
 `
